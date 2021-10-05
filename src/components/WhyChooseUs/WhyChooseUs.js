96905331110e940198b6c2f1ae61f34cfe72bfff
mdshohidulislam1486 <Shohidul Islam>
2021-10-05 23:28:42 +0600

    updated

--- src/components/WhyChooseUs/WhyChooseUs.js
+++ src/components/WhyChooseUs/WhyChooseUs.js
@@ -6,8 +6,8 @@ import './WhyChooseUs.css'
 const WhyChooseUs = (props) => {
     const {reson,description } = props.whyUs
     return (
-        <div className='my-grid mx-4'>
-            <h2 className='text-light'>{reson}</h2>
+        <div className='my-grid mx-4 mb-5'>
+            <h5 className='text-light'>{reson}</h5>
             <p className='text-warning'>{description}</p>
             <Button variant='warning'><Link className='about-btn' to='/about'>Contact us</Link></Button>
         </div>
